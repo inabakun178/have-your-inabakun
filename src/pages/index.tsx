026ea488f83ac6812b112fde20be1230dc4d8816
Hayato Inaba <hayato.inaba@nijibox.co.jp>
2022-06-25 20:48:31 +0900

    add: app.tsx, index.tsx 初期化

---
+++ src/pages/index.tsx
@@ -0,0 +1,16 @@
+import type { NextPage } from "next";
+import Head from "next/head";
+
+const Home: NextPage = () => {
+  return (
+    <>
+      {/* PageTemplate componentを作る */}
+      <Head>
+        <title>Have Your Inabakun</title>
+      </Head>
+      Top
+    </>
+  );
+};
+
+export default Home;
