a41fc8d7404c264a8944d0ad6f0e9478c2db4a3f
Hayato Inaba <hayato.inaba@nijibox.co.jp>
2022-12-02 01:03:37 +0900

    add: fv 追加

---
+++ src/components/pages/top/Fv/Fv.tsx
@@ -0,0 +1,15 @@
+import { Box, Flex, Image } from "@chakra-ui/react";
+
+const Fv = () => {
+  return (
+    <Flex
+      minH={{ base: "calc(100vh - 50px)", md: "calc(100vh - 100px)" }}
+      justifyContent="center"
+      alignItems="center"
+    >
+      <Image src="/fv_title.svg" alt="Have Your Inabakun" />
+    </Flex>
+  );
+};
+
+export default Fv;
