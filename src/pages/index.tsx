05956bca0bf65bb98999f54872d6bd3003aa55d5
NBX Hayato Inaba <1ok78ndo512@gmail.com>
2022-12-01 12:33:58 +0900

    fix: spのスタイル崩れ修正

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,7 +4,10 @@ import PageTemplate from "../components/common/PageTemplate/PageTemplate";
 
 const Home: NextPage = () => {
   return (
-    <Text fontSize="300px" color="rgba(255,255,255, 0.5)">
+    <Text
+      fontSize={{ base: "120px", md: "300px" }}
+      color="rgba(255,255,255, 0.5)"
+    >
       イナバくん準備中
     </Text>
   );
