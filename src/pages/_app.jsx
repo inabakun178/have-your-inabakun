54709e28baa1b3cd7e64fb786846ecf8818a01d6
NBX Hayato Inaba <1ok78ndo512@gmail.com>
2022-12-01 16:55:13 +0900

    add: カーソルエフェクトつけた

---
+++ src/pages/_app.jsx
@@ -0,0 +1,31 @@
+import "../styles/global.css";
+// import type { AppProps } from "next/app";
+import { ChakraProvider, extendTheme } from "@chakra-ui/react";
+import PageTemplate from "../components/common/PageTemplate/PageTemplate";
+import { chakraTheme } from "../lib/chakraTheme";
+import dynamic from "next/dynamic";
+
+const AnimatedCursor = dynamic(() => import("react-animated-cursor"), {
+  ssr: false,
+});
+
+function MyApp({ Component, pageProps }) {
+  return (
+    <ChakraProvider theme={extendTheme(chakraTheme)}>
+      <PageTemplate>
+        <Component {...pageProps} />
+      </PageTemplate>
+      <AnimatedCursor
+        color="255, 255, 255"
+        innerSize={10}
+        outerSize={20}
+        hasBlendMode={true}
+        outerStyle={{
+          mixBlendMode: "difference",
+        }}
+      />
+    </ChakraProvider>
+  );
+}
+
+export default MyApp;
