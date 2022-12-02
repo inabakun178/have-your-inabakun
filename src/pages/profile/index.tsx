2bf3fcf90dba86682edd0f707a1ffc9e5a37d35f
NBX Hayato Inaba <1ok78ndo512@gmail.com>
2022-12-02 12:39:02 +0900

    add: profile pageファイル作成

---
+++ src/pages/profile/index.tsx
@@ -0,0 +1,13 @@
+import type { NextPage } from "next";
+import { Box, Text } from "@chakra-ui/react";
+import PageTemplate from "../../components/common/PageTemplate/PageTemplate";
+
+const Profile: NextPage = () => {
+  return (
+    <PageTemplate pageTitle="Profile">
+      <Text>Profile</Text>
+    </PageTemplate>
+  );
+};
+
+export default Profile;
