4d7b6df095fc415133aefcebfb57b13c785dd671
NBX Hayato Inaba <1ok78ndo512@gmail.com>
2022-12-02 15:05:57 +0900

    add: framer-motionで簡単なページ遷移アニメーション作成

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -4,6 +4,7 @@ import { ChakraProvider, extendTheme } from "@chakra-ui/react";
 import PageTemplate from "../components/common/PageTemplate/PageTemplate";
 import { chakraTheme } from "../lib/chakraTheme";
 import dynamic from "next/dynamic";
+import { AnimatePresence } from "framer-motion";
 
 const AnimatedCursor = dynamic(() => import("react-animated-cursor"), {
   ssr: false,
@@ -12,7 +13,9 @@ const AnimatedCursor = dynamic(() => import("react-animated-cursor"), {
 function MyApp({ Component, pageProps }) {
   return (
     <ChakraProvider theme={extendTheme(chakraTheme)}>
-      <Component {...pageProps} />
+      <AnimatePresence mode="wait">
+        <Component {...pageProps} />
+      </AnimatePresence>
       <AnimatedCursor
         color="255, 255, 255"
         innerSize={10}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,4 @@
+import { motion } from "framer-motion";
 import type { NextPage } from "next";
 import PageTemplate from "../components/common/PageTemplate/PageTemplate";
 import Fv from "../components/pages/top/Fv/Fv";
